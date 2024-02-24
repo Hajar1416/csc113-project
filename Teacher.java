dcbda8e8ad0000b7d37aadf63c4dd1db3724f500
Hajar1416 <161123151+Hajar1416@users.noreply.github.com>
2024-02-24 23:48:46 +0300

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,14 +1,40 @@
 public class Teacher extends University{
+private int Hour ;
+
+public Teacher( String Uniname, String name, String id, int age, int hour) {
+super( Uniname, name , id , age  ) ;
+Hour = hour ;
+
+}
+
+public double salary(double hour){
+return Hour*100 ;
+
+}
+
+public String generateEmail() {
+
+int loc1 = UniName.indexOf(' ') ;
+String ch1 = UniName.substring(0,1) ;
+
+String ch2 = UniName.substring(loc1 + 1,loc1 + 2) ;
+int loc2 = UniName.indexOf(' ' , loc1 + 1) ;
+
+String ch3 = UniName.substring(loc2 + 1,loc2 + 2) ;
+
+String full = ch1+ch2+ch3 ;
+
+return Name+"@"+full+".edu.sa";
+
+}
 
 
 
-
-
-
-
-
-
-
+public void display(){
+super.display();
+System.out.println("\n Hour : " + Hour) ;
+}
 
 
 }
+
