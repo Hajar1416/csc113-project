c66437ccc0d5271c6519581847c528e1aaed7dbb
Hajar1416 <161123151+Hajar1416@users.noreply.github.com>
2024-02-26 12:46:48 +0300

    Update University.java

--- University.java
+++ University.java
@@ -1,21 +1,21 @@
 public abstract class  University
 {
-
+protected String UniName;
 protected String Name;
 protected String ID;
 protected int Age;
 
 
 
-public University(String name,String id,int age){
-
+public University( String Uniname,String name,String id,int age){
+UniName=Uniname;
 Name=name;
 ID=id;
 Age=age;
 }
 
 
-public abstract String genrateEmail();
+public abstract String generateEmail();
 
 
 public void display(){
