5cf639f877ecac4a7177209888648a37494658e4
Hajar1416 <161123151+Hajar1416@users.noreply.github.com>
2024-05-08 14:51:07 +0300

    Update College.java

--- College.java
+++ College.java
@@ -1,4 +1,7 @@
-public class College{
+import java.io.* ;
+import javax.swing.JOptionPane ;
+
+public class College implements Serializable{
 private String CollegeName ;
 private Student[] studentList ;
 private int numOfStudent ;
@@ -44,13 +47,69 @@ return studentList[i] ;
 return null ;
 }
 
-  public void display(){
+public String toString(){
+String str ="" ;
+for(int i=0 ; i<numOfStudent ; i++) {
+str = str + studentList[i].toString() + "\n" ; }
+return str;
+}
 
-for(int i=0 ; i<numOfStudent ; i++)
-studentList[i].display();
+public void save(String fileName){
+try {
+File save = new File(fileName);
+if(save.exists()) {
+int num = JOptionPane.showConfirmDialog(null, "this file is already exist do you want to countinue ? " , "alert" , JOptionPane.YES_NO_OPTION) ;
+if(num == JOptionPane.NO_OPTION)
+return ;
+}
+
+FileOutputStream out = new FileOutputStream(save);
+ObjectOutputStream file = new ObjectOutputStream(out);
+
+file.writeInt(numOfStudent) ;
+file.writeObject(CollegeName) ;
+
+for(int i = 0 ; i < numOfStudent ; i++)
+file.writeObject(studentList[i]) ;
+
+
+file.close() ;
+JOptionPane.showMessageDialog(null, "save all information is done");
+}
+catch(IOException ex) {
+JOptionPane.showMessageDialog(null, "error! in my project" + ex.toString());
+}
+
 
 }
 
+/*public void load(String fileName) {
+try {
+File load = new File(fileName);
+FileInputStream fileInput = new FileInputStream(load);
+ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+
+int size = objectInput.readInt() ;
+String name = (String) objectInput.readObject() ;
+
+CollegeName = name ;
+for(int i = 0 ; i < size ; i++) {
+Student obj = (Student)objectInput.readObject() ;
+this.addStudent(obj) ;
+
 }
 
+objectInput.close() ;
+JOptionPane.showMessageDialog(null, "all information has been uploaded");
+}
+catch(ClassNotFoundException ex) {
+JOptionPane.showMessageDialog(null, "error while reading object" ) ;
+}
+catch(IOException ex){
+JOptionPane.showMessageDialog(null, "error error while load file" + ex.toString());
+}
+
+}
+*/
+}
 
