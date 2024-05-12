3c1bf80b8c57508fabb12e316ff6fca1e9beaa35
Hajar1416 <161123151+Hajar1416@users.noreply.github.com>
2024-05-12 18:55:15 +0300

    Update AddStudentFrame.java

--- AddStudentFrame.java
+++ AddStudentFrame.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -30,53 +31,17 @@ public class AddStudentFrame extends JFrame {
         nameField = new JTextField(20);
         add(nameField);
         
-        String idtext=idField.getText();       
         
         
         JLabel idLabel = new JLabel("Student ID:");
         add(idLabel);
         idField = new JTextField(20);
         add(idField);
-        
-        try{
-        if( idtext.length() != 10 )
-        throw new InvalidIdException();
-         int id=Integer.parseInt(idtext);
-
-        }catch( NumberFormatException ex ){
-         JOptionPane.showMessageDialog(this, "invalid id, try again");
-         return;
-         }
-
-        catch( InvalidIdException ext ){
-        JOptionPane.showMessageDialog(this, ext.getMessage() );
-        return;}
-        
-
-        String agetext=ageField.getText();    
-
 
         JLabel ageLabel = new JLabel("Student Age:");
         add(ageLabel);
         ageField = new JTextField(20);
         add(ageField);
-        
-        try{
-       int age=Integer.parseInt(agetext);
-        if(age<=0)
-        throw new InvalidAgeException();
-        }catch( NumberFormatException ex ){
-         JOptionPane.showMessageDialog(this, "invalid age, try again");
-         return;
-         }
-         catch(InvalidAgeException ext ){
-          JOptionPane.showMessageDialog(this, ext.getMessage() );
-          return;
-          }
-
-      
-        
-        
 
         JLabel gradeLabel = new JLabel("Student Total Grade:");
         add(gradeLabel);
@@ -89,9 +54,31 @@ public class AddStudentFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String name = nameField.getText();
                 String id = idField.getText();
-                int age = Integer.parseInt(ageField.getText());
+                  int age=0;
                 double grade = Double.parseDouble(gradeField.getText());
+                                                
+                
+                try{
+               if( id.length() != 10 )
+               throw new InvalidIdException();
+         
+               }catch( InvalidIdException ext ){
+               JOptionPane.showMessageDialog(null,ext.toString() );
+               return;
+               }
+          
+               
+               try{
+                
+                 age = Integer.parseInt(ageField.getText());
+                 if(age<=0)
+                 throw new InvalidAgeException();
+                }catch(InvalidAgeException ex){
+                                 JOptionPane.showMessageDialog(null,ex.toString() );return;}
 
+
+
+                            
                 // Create a new student
                 Student student = new Student(name, id, age, grade);
 
@@ -99,28 +86,36 @@ public class AddStudentFrame extends JFrame {
                 college.addStudent(student);
 
                             // Display a message or perform any other desired action
-                JOptionPane.showMessageDialog(null, "Student added to Computer Science college.");
+                JOptionPane.showMessageDialog(null, "Student added to the college.");
             }
         });
         add(addButton);
-        
-        JButton saveButton = new JButton("Save Student Information");
+   
+        JButton saveButton = new JButton("Save and load Student Information");
 saveButton.addActionListener(new ActionListener() {
+
     @Override
     public void actionPerformed(ActionEvent e) {
+     
         college.save("File.txt"); // Call the save method in the College class
         college.load("File.txt");
-                    
-// Clear the input fields
+        
+           // Clear the input fields
                 nameField.setText("");
                 idField.setText("");
                 ageField.setText("");
                 gradeField.setText("");
+         
+               
               
 
     }
 });
 add(saveButton);
+
+
+                
+
 
 
         JButton searchButton = new JButton("Search for Student");
@@ -146,6 +141,5 @@ add(saveButton);
     revalidate();
 }
 
-
-
 }
+
